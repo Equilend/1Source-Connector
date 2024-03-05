63aaf8f26231f2d87c0d017544a9948a3aa1906e
Yevhen Yermolenko <yevhen.yermolenko@equilend.com>
2024-03-05 15:47:29 +0200

    merge release-004 changes

--- src/main/java/com/intellecteu/onesource/integration/config/security/OneSourceSecurityConfig.java
+++ src/main/java/com/intellecteu/onesource/integration/config/security/OneSourceSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.intellecteu.onesource.integration.config.security;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,15 +11,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-
+import org.springframework.web.cors.CorsConfiguration;
 
 @Slf4j
 @Configuration
 @EnableWebSecurity
 public class OneSourceSecurityConfig {
+
+    private final String uiClientId;
+
+    public OneSourceSecurityConfig(@Value("${onesource.auth.api.resource}") String uiClientId) {
+        this.uiClientId = uiClientId;
+    }
 
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
@@ -30,13 +39,21 @@ public class OneSourceSecurityConfig {
         http.authorizeHttpRequests(auth ->
             auth
                 .anyRequest()
-                .authenticated());
+                .hasRole(uiClientId));
 
         http.csrf(Customizer.withDefaults());
-        http.cors(Customizer.withDefaults());
+        // todo Remove or Configure according to the requirements after the demo
+        http.cors(cors -> cors.configurationSource(request -> {
+            CorsConfiguration corsConfig = new CorsConfiguration();
+            corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
+            corsConfig.setAllowedMethods(List.of("*"));
+            corsConfig.setAllowedHeaders(List.of("*"));
+            return corsConfig;
+        }));
 
-        http.oauth2ResourceServer((oauth2) -> oauth2
-            .jwt(Customizer.withDefaults()));
+        http.oauth2ResourceServer(oauth2 -> oauth2
+            .jwt(jwt -> jwt.jwtAuthenticationConverter(
+                new OneSourceJwtClaimsConverter(uiClientId, new JwtGrantedAuthoritiesConverter()))));
         return http.build();
     }
 
@@ -49,10 +66,14 @@ public class OneSourceSecurityConfig {
                 .permitAll());
 
         http.csrf(AbstractHttpConfigurer::disable);
-        http.cors(Customizer.withDefaults());
+        http.cors(cors -> cors.configurationSource(request -> {
+            CorsConfiguration corsConfig = new CorsConfiguration();
+            corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
+            corsConfig.setAllowedMethods(List.of("*"));
+            corsConfig.setAllowedHeaders(List.of("*"));
+            return corsConfig;
+        }));
 
-        http.oauth2ResourceServer((oauth2) -> oauth2
-            .jwt(Customizer.withDefaults()));
         return http.build();
     }
 }
